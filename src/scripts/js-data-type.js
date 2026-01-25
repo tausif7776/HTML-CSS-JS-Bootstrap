71eee99bc8fec63fa4af19e0cf6323bfebaca71c
tausif7776 <tausif.raza7776@gmail.com>
2026-01-24 23:14:44 -0500

    Boolean

--- src/scripts/js-data-type.js
+++ src/scripts/js-data-type.js
@@ -26,5 +26,28 @@
 // let data = "Ahmed";
 // console.log(data.split(""));
 
-let data = "      welcome           ";
-console.log(data.trim())
+// let data = "      welcome           ";
+// console.log(data.trim())
+
+// console.log(true+"A");
+// console.log(true-true);
+// console.log(true+19);
+
+// let x;
+// let y;
+// console.log(x);
+// console.log(y);
+
+// const userId = Symbol();
+// let user = {
+//     [userId]: "John",
+//     age: 22
+// }
+// for (let ans in user) {
+//     console.log(ans);
+// }
+// console.log(user[userId]);
+
+let arr = [10, 20, 30, 40];
+console.log(arr);
+console.log(arr[0]);
